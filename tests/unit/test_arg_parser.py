811298da4085688904ff266d87f46e6cc6f5f4c4
iacopoff <iacopo.ff@gmail.com>
2022-02-09 16:43:38 +0000

    init
    X

---
+++ tests/unit/test_arg_parser.py
@@ -0,0 +1,161 @@
+import pytest
+from climetlab_cems_flood.utils import Parser
+
+
+
+
+def assert_period(years,months,days,expected):
+
+    assert years == expected[0]
+    assert months == expected[1]
+    assert days == expected[2]
+
+def test_parser_leadtime():
+
+    parser = Parser()
+
+    assert parser.leadtime("24-72/480-600") == [
+        "24",
+        "48",
+        "72",
+        "480",
+        "504",
+        "528",
+        "552",
+        "576",
+        "600",
+    ]
+
+    assert parser.leadtime("24-600") == [
+        "24",
+        "48",
+        "72",
+        "96",
+        "120",
+        "144",
+        "168",
+        "192",
+        "216",
+        "240",
+        "264",
+        "288",
+        "312",
+        "336",
+        "360",
+        "384",
+        "408",
+        "432",
+        "456",
+        "480",
+        "504",
+        "528",
+        "552",
+        "576",
+        "600",
+    ]
+
+    assert parser.leadtime("24-72/240-336/480-600") == [
+        "24",
+        "48",
+        "72",
+        "240",
+        "264",
+        "288",
+        "312",
+        "336",
+        "480",
+        "504",
+        "528",
+        "552",
+        "576",
+        "600",
+    ]
+
+@pytest.mark.parametrize("string, expected",[
+                                                (
+                                                    "20180101-20181231/20200101-20201231",
+                                                    [
+                                                    ["2018", "2020"],
+                                                    ["%02d"%m for m in range(1,13)],
+                                                    ["%02d"%d for d in range(1,32)]
+                                                    ]
+                                                ),
+                                                                                                (
+                                                    "20180201-20180212/20200101-20200125",
+                                                    [
+                                                    ["2018", "2020"],
+                                                    ["01", "02"],
+                                                    ["%02d"%d for d in range(1,26)]
+                                                    ]
+                                                ),
+                                                 (
+                                                    "20180212/20200101",
+                                                    [
+                                                    ["2018", "2020"],
+                                                    ["01","02"],
+                                                    ["01","12"]
+                                                    ]
+                                                ),
+                                                 (
+                                                    "20180201",
+                                                    [
+                                                    ["2018"],
+                                                    ["02"],
+                                                    ["01"]
+                                                    ]
+                                                ),
+                                                (
+                                                    "20100101-20200325",
+                                                    [
+                                                    ["%d"%y for y in range(2010,2021,1)],
+                                                    ["%02d"%m for m in range(1,13)],
+                                                    ["%02d"%d for d in range(1,32)]
+                                                    ]
+                                                ),                                           
+                                                pytest.param("202001-12/*1010", 42, marks=pytest.mark.xfail)
+                                            ]
+                        ) # expected = [[years],[months],[days]]
+def test_parser_period(string,expected):
+    
+    parser = Parser()
+
+
+    years, months, days = parser.period(string)
+
+    assert_period(years,months,days,expected)
+
+@pytest.mark.parametrize("string, expected",[
+                                                (
+                                                    "*0101",
+                                                    [
+                                                    ["%d"%y for y in range(1979,2022,1)],
+                                                    ["01"],
+                                                    ["01"]
+                                                    ]
+                                                ),
+                                                                                                (
+                                                    "***",
+                                                    [
+                                                    ["%d"%y for y in range(1979,2022,1)],
+                                                    ["%02d"%m for m in range(1,13)],
+                                                    ["%02d"%d for d in range(1,32)]
+                                                    ]
+                                                ),
+                                                                                                (
+                                                    "2020*01",
+                                                    [
+                                                    ["2020"],
+                                                    ["%02d"%m for m in range(1,13)],
+                                                    ["01"]
+                                                    ]
+                                                ),  
+                                            ]
+                                                
+                        )
+def test_parser_period_star_param(string,expected):
+
+    parser = Parser([1979,2021])
+
+    years, months, days = parser.period(string)
+
+    assert_period(years,months,days,expected)
